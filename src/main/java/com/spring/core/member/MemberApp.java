03af85de296ee35a41dd74896059356849ab7953
오한별 <suriessay@naver.com>
2020-11-26 21:29:25 +0900

    pojo 콘솔 테스트

---
+++ src/main/java/com/spring/core/member/MemberApp.java
@@ -0,0 +1,13 @@
+package com.spring.core.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memA",Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
---
+++ src/test/java/com/spring/core/member/MemberServiceTest.java
@@ -0,0 +1,4 @@
+package com.spring.core.member;
+
+public class MemberServiceTest {
+}
