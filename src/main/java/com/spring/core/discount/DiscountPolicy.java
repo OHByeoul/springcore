c55f7e3062c711dd068fc9251fea84cd1915eda2
오한별 <suriessay@naver.com>
2020-11-29 23:26:03 +0900

    order서비스, discount 서비스 작성

---
+++ src/main/java/com/spring/core/discount/DiscountPolicy.java
@@ -0,0 +1,7 @@
+package com.spring.core.discount;
+
+import com.spring.core.member.Member;
+
+public interface DiscountPolicy {
+    int discount(Member member, int price);
+}
--- src/test/java/com/spring/core/member/MemberServiceTest.java
+++ src/test/java/com/spring/core/member/MemberServiceTest.java
@@ -1,4 +1,20 @@
 package com.spring.core.member;
 
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        Member member = new Member(1L, "memA",Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
 }
