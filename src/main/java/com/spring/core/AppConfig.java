52cba9a9f8d069f4d02ebac38c8f1f97566ad264
오한별 <suriessay@naver.com>
2020-12-01 23:19:39 +0900

    테스트 부분 생성,주입 부분과 구현부분 분리, appconfig 리팩토링

--- src/main/java/com/spring/core/AppConfig.java
+++ src/main/java/com/spring/core/AppConfig.java
@@ -8,11 +8,19 @@ import com.spring.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    private MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 }
