8d68d6c84edd4622b23424af4abc088fbd56734b
오한별 <suriessay@naver.com>
2020-12-02 23:45:05 +0900

    기존소스를 스프링기반으로 변경 - 스프링 컨테이너에 빈으로 등록

--- src/main/java/com/spring/core/AppConfig.java
+++ src/main/java/com/spring/core/AppConfig.java
@@ -5,20 +5,27 @@ import com.spring.core.discount.FixDiscountPolicy;
 import com.spring.core.member.*;
 import com.spring.core.order.OrderService;
 import com.spring.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(getMemberRepository());
     }
 
-    private MemberRepository getMemberRepository() {
+    @Bean
+    public MemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
